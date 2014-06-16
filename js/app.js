6dba8c0b72b310c46f75731e41feec92e15b6f2b
TheRedeemed <abdoulkaders77@gmail.com>
2014-06-15 22:29:24 -0400

    Hot or cold

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+	
 	var randNum;
 	var inputNum;
 	var feedBackMsg;
@@ -16,16 +17,16 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-
+	
 	function guessNumber(){
 		var genNum;
 		genNum = Math.floor((Math.random() * 100) + 1);
 		return genNum;
 	}
-
+	
 	randNum = guessNumber();
-
-	alert(randNum);
+	
+	//alert(randNum);
 	/*
 	$('#userGuess').keyup(function(e){
 		 var key = e.keyCode
@@ -35,12 +36,12 @@ $(document).ready(function(){
 			compareNumber(randNum);
 		};
 	});	*/
-
+	
 	$('#guessButton').click(function(){
 		//alert("Button Click");
 		compareNumber(randNum);
 	}); 
-
+	
 	$('.new').click(function(){
 		location.reload();
 	});
@@ -51,64 +52,57 @@ $(document).ready(function(){
 		var playAgain = true;
 
 		numFormatValidation(inputNum);
-
+		
 			if(numValueValidation(inputNum)){
-
+				
 				if(inputNum < num){
 					$('#guessList').append('<li class="cold">'+inputNum+'</li>');
 					//$('#guessList').append('Cold');
 					feedBackMsg = 'Cold!!';
 					feedBackUpdate(feedBackMsg);
 					counter();
-
-
+					
+				
 				}else if(inputNum > num){
 					$('#guessList').append('<li class="hot">'+inputNum+'<li>');
 					//$('#guessList').append('Hot');
 					feedBackMsg = 'Hot!!';
 					feedBackUpdate(feedBackMsg);
 					counter();				
-
+					
 				} else if (inputNum == num){
 					//alert('Got It!!!');
 					feedBackMsg = 'Got It!!!';
 					feedBackUpdate(feedBackMsg);
 					do{
 						playAgain = confirm("Would like to play again");
-
+					
 						if(playAgain === true){
 							playAgain = false;
 							location.reload();
-
+							
 						}else{
 							playAgain = false;
 							$('#userGuess').prop('disabled', true);
 							$('#guessButton').prop('disabled', true);
-							$( "#guessButton" ).hover(
-							  function() {
-							    $( this ).addClass( "buttonDisabled" );
-							  }, function() {
-							    $( this ).removeClass( "buttonDisabled" );
-							  }
-							);
 						}
 					} while(playAgain);
 				}
-
+				
 				$('#userGuess').val("");
 				$('#userGuess').empty();		
-
+				
 			}
 
 	}
-
+	
 	function numFormatValidation(input){
-
+		
 		if(isNaN(input)){
 				//alert(input + ' is not a number\nPlease enter a valid number');
 				//$('#feedback').text("");
 				//$('#feedback').append(input + ' is not a valid number <br> Please enter a valid number');
-				feedBackMsg = '<span id="nonDigit">'+input + '</span> is not a valid number <br> Please enter a valid number';
+				feedBackMsg = input + ' is not a valid number <br> Please enter a valid number';
 				feedBackUpdate(feedBackMsg);
 				$('#userGuess').val("");
 				$('#userGuess').empty();
@@ -116,9 +110,9 @@ $(document).ready(function(){
 			return input;
 		}		
 	}
-
+	
 	function numValueValidation(input){
-
+		
 		if(input < 1 || input > 100){
 				//alert('Please enter a number between 1 and 100');
 				//$('#feedback').text("");
@@ -137,7 +131,7 @@ $(document).ready(function(){
 		$('#count').text("");
 		$('#count').append(++guess);
 	}
-
+	
 	function feedBackUpdate(msg){
 		$('#feedback').text("");
 		$('#feedback').append(msg);
